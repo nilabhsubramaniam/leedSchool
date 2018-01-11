ea3e7c2b3587206e8d9c6cd16229a2c44452e636
Nilabh-Subramaniam <=>
2018-01-11 13:01:12 +0530

    add and delete

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,18 @@
  * MainCtrl - controller
  */
 function MainCtrl($scope, $state,toaster) {
+    $scope.addClassFormData ={
+        Id: 0,
+        fullName: 0,
+        dob: '',
+        school:'',
+        class:'',
+        divison:'',
+        status:''
+    };
 	$scope.classList =[
 			{
-                        _id: 0,
+                        Id: 0,
                         fullName: 'Rohan Swarnkar',
                         dob: '22-07-1993', 
                         school: 'St Mary school' ,
@@ -13,7 +22,7 @@ function MainCtrl($scope, $state,toaster) {
                         status:'active'
             },
             {
-                        _id: 1,
+                        Id: 1,
                         fullName: 'Arpit Bhatt',
                         dob: '22-05-1997', 
                         school: 'St karen seconary school' ,
@@ -22,7 +31,7 @@ function MainCtrl($scope, $state,toaster) {
                         status:'active'
             },
             {
-                        _id: 2,
+                         Id: 2,
                         fullName: 'Arpit chaturvedi',
                         dob: '22-03-1991', 
                         school: 'St john x111' ,
@@ -33,23 +42,33 @@ function MainCtrl($scope, $state,toaster) {
 	]
 
  //Add class 
- $scope.submitClassInfo = function (addClassFormData) {
-         var tmp = {"_id": 0, "fullName": '', "dob": '', "school": '' ,"class":'',"divison":'',"status":''};
-        tmp._id = addClassFormData._id + 1;
-        $scope.classList.push(tmp);
+ $scope.submitClassInfo = function () {
+     var _class = {
+         Id: $scope.classList.length + 1,
+         fullName: $scope.addClassFormData.fullName,
+         dob: $scope.addClassFormData.dob,
+         school : $scope.addClassFormData.school,
+         class: $scope.addClassFormData.class,
+         divison: $scope.addClassFormData.divison,
+         status: $scope.addClassFormData.status
+     };
+     $scope.classList.push(_class);
+     ClearModel();
+ };
+    //Delete Class
+    $scope.deleteClass = function(classDetail){
+        var _index = $scope.classList.indexOf(classDetail);
+        $scope.classList.splice(_index, 1);
     };
-
  //Edit class
 
  $scope.editClass = function(){
 
- }
+ };
 
  //Delete class 
 
- $scope.deleteClass = function (index){
- 			$scope.classList.splice(index, 1);
- };
+
 
 }
 
